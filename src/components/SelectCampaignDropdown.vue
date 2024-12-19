3efaefde37b773093327e65fedfb5a20642701af
Kate Smith <kate.smith@psi.ch>
2024-12-19 04:54:36 +0100

    v0.5.18 WIP for SFX and FFCS

---
+++ src/components/SelectCampaignDropdown.vue
@@ -0,0 +1,42 @@
+<template>
+    <n-select
+      :options="options"
+      :default-value="options[0]['value']"
+      :consistent-menu-width="false"
+      @update:value="handleSelect"
+    >
+    </n-select>
+</template>
+
+<script setup>
+import { NSelect } from "naive-ui";
+import { ref } from "vue";
+
+const campaigns = [
+      ]
+
+
+function convertCampaignsToOptions() {
+  var stack = campaigns;
+  var stackLength = stack.length - 1;
+  var out = [];
+  for (var i = 0; i <= stackLength; i++) {
+    var label = stack[i];
+    var value = stack[i];
+    var tmp = { label: label, value: value };
+    out.push(tmp);
+  }
+  return out;
+}
+
+const options = convertCampaignsToOptions();
+
+const selected = ref([]);
+
+const emit = defineEmits(["selectedCampaign"]);
+
+function handleSelect(key) {
+selected.value = String(key);
+emit("selectedCampaign", String(key));
+}
+</script>  
